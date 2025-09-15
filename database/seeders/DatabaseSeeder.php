0f3352316261875104e693b918aaaad3482522e8
Dexter.Bagtang <dexterbagtang@gmail.com>
2025-09-15 10:51:16 +0800

    refactor: Improve theme handling and seeders
    The default appearance is set to light.
    
    PurchaseOrderSeeder and InvoiceSeeder are commented out.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,8 +28,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             VendorSeeder::class,
             ProjectSeeder::class,
-            PurchaseOrderSeeder::class,
-            InvoiceSeeder::class,
+//            PurchaseOrderSeeder::class,
+//            InvoiceSeeder::class,
         ]);
 
 
